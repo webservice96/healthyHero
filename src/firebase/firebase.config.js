e2d47264bb34607d59ee9b3ccee28e2466560876
Helal Uddin <35348437+webservice96@users.noreply.github.com>
2021-11-27 11:03:36 +0600

    Update firebase.config.js

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCqfYMBaIkGJLqGQw0GAN59YtVtiWd-90s",
-    authDomain: "healthy-hero-a4215.firebaseapp.com",
-    projectId: "healthy-hero-a4215",
-    storageBucket: "healthy-hero-a4215.appspot.com",
-    messagingSenderId: "897760620310",
-    appId: "1:897760620310:web:a78620ea3f95fb12fcea37"
+    apiKey: ,
+    authDomain: ,
+    projectId: ,
+    storageBucket: ,
+    messagingSenderId: ,
+    appId: 
 };
 
 export default firebaseConfig;
